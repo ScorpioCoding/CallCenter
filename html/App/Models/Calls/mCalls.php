53d122c9e1d1caa6563f3dafc84bb97d97e1984a
denOldTimer <kribo@outlook.com>
2023-02-22 11:19:59 +0100

    add Models

---
+++ html/App/Models/Calls/mCalls.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Models\Calls;
+
+use PDO;
+use PDOException;
+
+use App\Core\Database;
+
+use App\Core\NewException;
+
+
+
+class mCalls extends Database
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+
+
+  //
+  //END CLASS
+}
---
+++ html/App/Models/Company/mContacts.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Models\Company;
+
+use PDO;
+use PDOException;
+
+use App\Core\Database;
+
+use App\Core\NewException;
+
+
+
+class mContacts extends Database
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+
+
+  //
+  //END CLASS
+}
