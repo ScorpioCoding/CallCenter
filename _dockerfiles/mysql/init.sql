90ffad438969441939a8784ebc442dfcf87e3f0c
denOldTimer <kribo@outlook.com>
2023-02-22 11:13:53 +0100

    Update Mysql Init File

--- _dockerfiles/mysql/init.sql
+++ _dockerfiles/mysql/init.sql
@@ -11,3 +11,57 @@ CREATE TABLE IF NOT EXISTS User (
   psw_hash VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+
+CREATE TABLE IF NOT EXISTS Company (
+  id SERIAL PRIMARY KEY,
+  comType ENUM ('customer','vendor'),
+  comName VARCHAR(50),  
+  comEmail VARCHAR(255),
+  comPhone VARCHAR(20),
+  comVat VARCHAR(20),
+  userId BIGINT UNSIGNED
+);
+
+CREATE TABLE IF NOT EXISTS Address (
+  id SERIAL PRIMARY KEY,
+  comType ENUM ('customer','vendor'),  
+  comName VARCHAR(255),
+  addtype ENUM ('location','billing','warehouse'),
+  addLine1 VARCHAR(255),
+  addLline2 VARCHAR(255),
+  addPostal VARCHAR(50),
+  addCity VARCHAR(255),
+  addState VARCHAR(255),
+  addCountry VARCHAR(255),
+  companyId BIGINT UNSIGNED,
+  userId BIGINT UNSIGNED
+);
+
+CREATE TABLE IF NOT EXISTS Contacts (
+  id SERIAL PRIMARY KEY,
+  comType ENUM ('customer','vendor'),
+  comName VARCHAR(255),
+  conFullName VARCHAR(255),
+  conGender VARCHAR(50),
+  conFname VARCHAR(255),
+  conLname VARCHAR(255),  
+  conEmail VARCHAR(255),
+  conPhone VARCHAR(255),
+  companyId BIGINT UNSIGNED,
+  userId BIGINT UNSIGNED
+);
+
+CREATE TABLE IF NOT EXISTS Calls (
+  id SERIAL PRIMARY KEY,
+  comType ENUM ('customer','vendor'),
+  comName VARCHAR(255),
+  conFullName VARCHAR(255),
+  callStatus VARCHAR(50),
+  callLastDate DATE,
+  callNextDate Date,
+  callLog TEXT,
+  contactId BIGINT UNSIGNED,
+  userId BIGINT UNSIGNED
+);
+
